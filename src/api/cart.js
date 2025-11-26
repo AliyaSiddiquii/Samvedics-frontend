aea75fa904a043dc2d84f772f870903d2f052894
AliyaSiddiquii <siddiquialiya686@gmail.com>
2025-11-27 03:01:01 +0530

    Update cart.js

--- src/api/cart.js
+++ src/api/cart.js
@@ -2,7 +2,10 @@
 import axios from 'axios';
 import { getStoredToken } from './auth';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000"
+});
+
 
 const getAuthHeader = () => {
     const token = getStoredToken();
